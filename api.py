faf7bc078e6a3b89ee19aec71ac31b0473e37d1d
MCommander2077 <2679935111@qq.com>
2023-01-05 12:13:27 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,20 +1,17 @@
-if __name__ == "__main__":
-    import requests
-    def Recognize_text(img):
-        password='8907'
-        url = "http://www.iinside.cn:7001/api_req"
-        filePath=img
-        text = ""
-        data={                                   
-            'password':password,
-            'reqmode':'ocr_pp'
-        }
-        files=[('image_ocr_pp',('wx.PNG',open(filePath,'rb'),'application/octet-stream'))]
-        headers = {}
-        response = requests.post( url, headers=headers, data=data, files=files).text
-        for i in eval(response)["data"]:
-            text = text + str(i)
-        return text
+from PyQt5.QtWidgets import QApplication
 
-    text = Recognize_text(r"C:\Users\MadYang\PycharmProjects\pythonProject2\搜题\小题题.JPG")
-    print(text)
+from PyQt5.QtGui import *
+
+import win32gui
+
+import sys
+
+hwnd = win32gui.FindWindow(None, 'C:\Windows\system32\cmd.exe')
+
+app = QApplication(sys.argv)
+
+screen = QApplication.primaryScreen()
+
+img = screen.grabWindow(hwnd).toImage()
+
+img.save("screenshot.jpg")
